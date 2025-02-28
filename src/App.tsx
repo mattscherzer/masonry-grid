3016ccefc411235cab01c6336ea49b4103aec3f7
Fritz von Scherzer <5264280+mattscherzer@users.noreply.github.com>
2025-02-28 21:59:23 +0100

    fetch pexels api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
+// TODO: update to actual API response type
+interface ApiResponse {
+  data: any[];
+}
+
 function App() {
+
+  const fetchPhotos = async () => {
+    try {
+      const apiKey = process.env.REACT_APP_PEXELS_API_KEY;
+      if(!apiKey) {
+        throw new Error('API key not found');
+      }
+
+      const response = await axios.get<ApiResponse>(`https://api.pexels.com/v1/curated?per_page=15`, {
+        headers: {
+          Authorization: apiKey
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching photos', error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <a
+        <button 
           className="App-link"
-          href="#"
-          rel="noopener noreferrer"
+          onClick={fetchPhotos}
         >
           Click here to load photos
-        </a>
+        </button>
       </header>
     </div>
   );
