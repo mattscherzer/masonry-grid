1af176f31c8ea81738dcd0be6aeaa330a9c55129
Fritz von Scherzer <5264280+mattscherzer@users.noreply.github.com>
2025-02-28 21:45:02 +0100

    Add typescript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="#"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to load photos
         </a>
       </header>
     </div>
